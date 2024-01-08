4c8be735d43cb2a49d94eef97d35a7b686f1493a
dirokim <dirokim7@gmail.com>
2024-01-08 17:53:17 +0900

    ad

---
+++ src/test/java/product/ProductDAOTest.java
@@ -0,0 +1,72 @@
+package product;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.winter.app.MyTest;
+import com.winter.app.product.ProductDAO;
+import com.winter.app.product.ProductDTO;
+
+public class ProductDAOTest extends MyTest {
+	
+	@Autowired
+	private ProductDAO productDAO;
+	
+	
+	
+	@Test
+	public void getAddTest() throws Exception {
+		ProductDTO productDTO = new ProductDTO();
+		int result= 0;
+		for(int i=0;i<100;i++) {
+		productDTO.setProductName("Test"+i);
+		productDTO.setProductContents("Test"+i);
+		productDTO.setProductRate(1.0+i);
+		productDTO.setProductJumsu(1.0+i);
+		 result = productDAO.add(productDTO);
+		if(i%10==0) {
+			Thread.sleep(500);
+		}
+		
+		}
+		System.out.println("100개 입력 완료");
+		assertEquals(1, result);
+	}
+	
+//	@Test
+//	public void listTest() throws Exception {
+//		
+//	 List<ProductDTO>ar = productDAO.list();
+//	  assertEquals(10,ar.size());
+//	}
+//	
+//	@Test
+//	public void deleteTest() throws Exception {
+//		ProductDTO productDTO = new ProductDTO();
+//		productDTO.setProductNum(21L);
+//	 int result = productDAO.delete(productDTO);
+//		assertEquals(1, result);
+//	}
+//	@Test
+//	public void detailTest() throws Exception {
+//		ProductDTO productDTO = new ProductDTO();
+//		productDTO.setProductNum(1L);
+//		productDAO.delete(productDTO);
+//		assertNotNull(productDTO);
+//	}
+//	@Test
+//	public void updateTest() throws Exception {
+//		ProductDTO productDTO = new ProductDTO();
+//		productDTO.setProductName("Test");
+//		productDTO.setProductContents("Test");
+//		productDTO.setProductRate(2.1);
+//		productDTO.setProductJumsu(3.1);
+//		int result= productDAO.update(productDTO);
+//		assertEquals(1, result);
+//	}
+}
