3b9a38bfc27f1c405dbf0c9d73e0d8e9e931a118
dirokim <dirokim7@gmail.com>
2024-01-12 23:32:09 +0900

    ad

--- src/main/java/com/winter/app/board/BoardDAO.java
+++ src/main/java/com/winter/app/board/BoardDAO.java
@@ -10,11 +10,19 @@ public interface BoardDAO {
 	//목록 가져오기 
 	public List<BoardDTO> getList() throws Exception;
 	
+	
+	
 	public BoardDTO getDetail(BoardDTO boardDTO) throws Exception;
+	
+	
 	
 	public int setAdd(BoardDTO boardDTO) throws Exception;
 	
+	
+	
 	public int setUpdate(BoardDTO boardDTO) throws Exception;
+	
+	
 	
 	public int setDelete (BoardDTO boardDTO) throws Exception;
 }
