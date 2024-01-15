ab9c99e0f6bb73eb00738ebcf3034e8ff3ec8057
dirokim <dirokim7@gmail.com>
2024-01-15 22:48:55 +0900

    ad

--- src/main/java/com/winter/app/util/FileManager.java
+++ src/main/java/com/winter/app/util/FileManager.java
@@ -1,9 +1,27 @@
 package com.winter.app.util;
 
+import java.io.File;
+import java.util.Calendar;
+
 import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
 
 @Component
 public class FileManager {
 	
+	public String fileSave(String path,MultipartFile file) throws Exception {
+		
+		File f = new File(path);
+		if(f.exists()) {
+			
+		}else {
+			f.mkdir();
+		}
+		Calendar ca = Calendar.getInstance();
+		String fileName = ca.getTimeInMillis()+"_"+file.getOriginalFilename();
+		f= new File(f,fileName);
+		file.transferTo(f);
+		return fileName;
+	}
 	
 }
