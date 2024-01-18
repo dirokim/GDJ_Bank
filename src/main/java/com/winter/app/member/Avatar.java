984c59ba80dc05954079f3be035c76a5fb2d7930
dirokim <dirokim7@gmail.com>
2024-01-18 11:03:36 +0900

    ad

---
+++ src/main/java/com/winter/app/member/Avatar.java
@@ -0,0 +1,26 @@
+package com.winter.app.member;
+
+import com.winter.app.files.FileDTO;
+
+public class Avatar extends FileDTO {
+		
+	private String userName;
+	
+	
+	
+	
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+	
+	
+	
+	
+	
+}
