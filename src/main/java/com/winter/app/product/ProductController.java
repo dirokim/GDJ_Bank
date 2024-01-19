b726208b68071c0152bf7e1cbcc9e583461e62d5
dirokim <dirokim7@gmail.com>
2024-01-19 23:49:37 +0900

    ad

--- src/main/java/com/winter/app/product/ProductController.java
+++ src/main/java/com/winter/app/product/ProductController.java
@@ -16,58 +16,64 @@ import com.winter.app.util.Pager;
 @RequestMapping(value = "/product/*")
 public class ProductController {
 	
+	
 	@Autowired
 	private ProductService productService;
 	
-	@RequestMapping(value="list",method=RequestMethod.GET)
-	public ModelAndView setList (Pager pager) throws Exception {	
+	@RequestMapping(value = "list", method = RequestMethod.GET)
+	public ModelAndView setList(Pager pager) throws Exception {
 		ModelAndView mv = new ModelAndView();
 		List<ProductDTO> ar = productService.serList(pager);
-		mv.addObject("list",ar);
-		mv.addObject("pager",pager);
+		mv.addObject("list", ar);
+		mv.addObject("pager", pager);
 		mv.setViewName("product/list");
 		return mv;
 	}
-	@RequestMapping(value="detail",method=RequestMethod.GET)
+
+	@RequestMapping(value = "detail", method = RequestMethod.GET)
 	public ModelAndView setDetail(ProductDTO productDTO) throws Exception {
 		productDTO = productService.serDetail(productDTO);
 		ModelAndView mv = new ModelAndView();
-		mv.addObject("dto",productDTO);
+		mv.addObject("dto", productDTO);
 		mv.setViewName("product/detail");
 		return mv;
 	}
-	@RequestMapping(value="add",method=RequestMethod.GET)
-	public void setAdd()throws Exception {
-		
+
+	@RequestMapping(value = "add", method = RequestMethod.GET)
+	public void setAdd() throws Exception {
+
 		return;
 	}
-	@RequestMapping(value="add",method=RequestMethod.POST)
-	public ModelAndView getAdd(ProductDTO productDTO,MultipartFile file)throws Exception{
-		int result = productService.serAdd(productDTO,file);
+
+	@RequestMapping(value = "add", method = RequestMethod.POST)
+	public ModelAndView getAdd(ProductDTO productDTO, MultipartFile file) throws Exception {
+		int result = productService.serAdd(productDTO, file);
 		String msg = "등록 실패";
-		if(result>0) {
+		if (result > 0) {
 			msg = "등록 성공";
 		}
 		ModelAndView mv = new ModelAndView();
-		mv.addObject("msg",msg);
-		mv.addObject("path","product/list");
+		mv.addObject("msg", msg);
+		mv.addObject("path", "product/list");
 		mv.setViewName("commons/result");
 		return mv;
 	}
-	@RequestMapping(value="update",method=RequestMethod.GET)
-	public void setUpdate() throws Exception{
+
+	@RequestMapping(value = "update", method = RequestMethod.GET)
+	public void setUpdate() throws Exception {
 		return;
 	}
-	@RequestMapping(value="update",method=RequestMethod.POST)
-	public ModelAndView getUpdate(ProductDTO productDTO) throws Exception{
+
+	@RequestMapping(value = "update", method = RequestMethod.POST)
+	public ModelAndView getUpdate(ProductDTO productDTO) throws Exception {
 		int result = productService.serUpdate(productDTO);
 		ModelAndView mv = new ModelAndView();
 		String msg = "수정실패";
-		if(result>0) {
+		if (result > 0) {
 			msg = "수정 성공";
 		}
-		mv.addObject("msg",msg);
-		mv.addObject("path","product/list");
+		mv.addObject("msg", msg);
+		mv.addObject("path", "product/list");
 		mv.setViewName("commons/result");
 		return mv;
 	}
