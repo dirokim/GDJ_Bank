2fd0c103b5e6d31b039c4b655c42fe03275eb2e9
dirokim <dirokim7@gmail.com>
2024-01-19 23:50:40 +0900

    ad

--- src/main/java/com/winter/app/board/notice/NoticeService.java
+++ src/main/java/com/winter/app/board/notice/NoticeService.java
@@ -27,74 +27,67 @@ public class NoticeService implements BoardService {
 	@Autowired
 	private ServletContext servletContext;
 	
-	
 	@Override
 	public List<BoardDTO> getList(Pager pager) throws Exception {
 
 		pager.makeRow();
 		Long totalCount = noticeDAO.getTotalCount(pager);
-		if(totalCount<1) {
-			totalCount=1L;
+		if (totalCount < 1) {
+			totalCount = 1L;
 		}
 		pager.makePage(totalCount);
-		List<BoardDTO>ar = noticeDAO.getList(pager);
-		return ar ; 
+		List<BoardDTO> ar = noticeDAO.getList(pager);
+		return ar;
 	}
-	
 
 	@Override
 	public BoardDTO getDetail(BoardDTO boardDTO) throws Exception {
-			
+
 		return noticeDAO.getDetail(boardDTO);
 	}
 
 	@Override
-	public int setAdd(BoardDTO boardDTO,MultipartFile[]attachs) throws Exception {
-			int result = noticeDAO.setAdd(boardDTO);
-			String path = servletContext.getRealPath("/resources/upload/notice");
-			
-			for(MultipartFile f :attachs) {
-			if(f.isEmpty()) {
+	public int setAdd(BoardDTO boardDTO, MultipartFile[] attachs) throws Exception {
+		int result = noticeDAO.setAdd(boardDTO);
+		String path = servletContext.getRealPath("/resources/upload/notice");
+
+		for (MultipartFile f : attachs) {
+			if (f.isEmpty()) {
 				continue;
 			}
-				
+
 			String fileName = fileManager.fileSave(path, f);
 			BoardFileDTO boardFileDTO = new BoardFileDTO();
 			boardFileDTO.setFileName(fileName);
 			boardFileDTO.setOriName(f.getOriginalFilename());
 			boardFileDTO.setNoticeNum(boardDTO.getNoticeNum());
 			result = noticeDAO.setFileAdd(boardFileDTO);
-			}
-		
-			
-		return result ;
+		}
+
+		return result;
 	}
 
 	@Override
-	public int setUpdate(BoardDTO boardDTO,MultipartFile[]attachs) throws Exception {
+	public int setUpdate(BoardDTO boardDTO, MultipartFile[] attachs) throws Exception {
 		return noticeDAO.setUpdate(boardDTO);
 	}
 
 	@Override
 	public int setDelete(BoardDTO boardDTO) throws Exception {
-	List<BoardFileDTO> ar =	noticeDAO.getFileList(boardDTO);
-	String path = servletContext.getRealPath("/resources/upload/notice");	
-	for(BoardFileDTO d: ar) {
-		
-			
+		List<BoardFileDTO> ar = noticeDAO.getFileList(boardDTO);
+		String path = servletContext.getRealPath("/resources/upload/notice");
+		for (BoardFileDTO d : ar) {
+
 			fileManager.fileDelete(path, d.getFileName());
 
 		}
-	
-	
+
 		noticeDAO.setFileDelete(boardDTO);
-			
+
 		int result = noticeDAO.setDelete(boardDTO);
-		
+
 		return result;
 	}
-
-	
 	
 	
 }
