1a0df2d322123ebab8a183a239b151eec810390c
dirokim <dirokim7@gmail.com>
2024-01-19 23:51:09 +0900

    ad

--- src/main/java/com/winter/app/board/notice/NoticeDAO.java
+++ src/main/java/com/winter/app/board/notice/NoticeDAO.java
@@ -17,48 +17,49 @@ public class NoticeDAO implements BoardDAO {
 	@Autowired
 	private SqlSession sqlSession;
 	private final String NAMESPACE = "com.winter.app.board.notice.NoticeDAO.";
-	
+
 	@Override
 	public Long getTotalCount(Pager pager) throws Exception {
-		
-		return sqlSession.selectOne(NAMESPACE+"getTotalCount");
+
+		return sqlSession.selectOne(NAMESPACE + "getTotalCount");
 	}
 
 	@Override
 	public List<BoardDTO> getList(Pager pager) throws Exception {
-		return sqlSession.selectList(NAMESPACE+"getList",pager);
+		return sqlSession.selectList(NAMESPACE + "getList", pager);
 	}
 
 	@Override
 	public BoardDTO getDetail(BoardDTO boardDTO) throws Exception {
 		// TODO Auto-generated method stub
-		return sqlSession.selectOne(NAMESPACE+"getDetail",boardDTO);
+		return sqlSession.selectOne(NAMESPACE + "getDetail", boardDTO);
 	}
 
 	@Override
 	public int setAdd(BoardDTO boardDTO) throws Exception {
-		return sqlSession.insert(NAMESPACE+"setAdd",boardDTO);
+		return sqlSession.insert(NAMESPACE + "setAdd", boardDTO);
 	}
 
 	@Override
 	public int setUpdate(BoardDTO boardDTO) throws Exception {
-		
-		return sqlSession.update(NAMESPACE+"setUpdate",boardDTO);
+
+		return sqlSession.update(NAMESPACE + "setUpdate", boardDTO);
 	}
 
 	@Override
 	public int setDelete(BoardDTO boardDTO) throws Exception {
-		return sqlSession.delete(NAMESPACE+"setDelete",boardDTO);
-	}
-	public List<BoardFileDTO> getFileList(BoardDTO boardDTO) throws Exception {
-		return sqlSession.selectOne(NAMESPACE+"getFileList",boardDTO);
-	}
-	
-	public int setFileAdd (BoardFileDTO boardFileDTO) throws Exception {
-		return sqlSession.insert(NAMESPACE+"setFileAdd",boardFileDTO);
+		return sqlSession.delete(NAMESPACE + "setDelete", boardDTO);
 	}
 
-	public int setFileDelete (BoardDTO boardDTO)throws Exception {
-		return sqlSession.delete(NAMESPACE+"setFileDelete",boardDTO);
+	public List<BoardFileDTO> getFileList(BoardDTO boardDTO) throws Exception {
+		return sqlSession.selectOne(NAMESPACE + "getFileList", boardDTO);
+	}
+
+	public int setFileAdd(BoardFileDTO boardFileDTO) throws Exception {
+		return sqlSession.insert(NAMESPACE + "setFileAdd", boardFileDTO);
+	}
+
+	public int setFileDelete(BoardDTO boardDTO) throws Exception {
+		return sqlSession.delete(NAMESPACE + "setFileDelete", boardDTO);
 	}
 }
