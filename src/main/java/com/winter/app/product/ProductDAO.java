ad3aeabdad05fa7b115f35bedddeb041ba02c01b
dirokim <dirokim7@gmail.com>
2024-01-19 23:49:17 +0900

    ad

--- src/main/java/com/winter/app/product/ProductDAO.java
+++ src/main/java/com/winter/app/product/ProductDAO.java
@@ -15,20 +15,24 @@ public class ProductDAO {
 	private SqlSession sqlSession;
 	private final String NAMESPACE ="com.winter.app.product.ProductDAO." ;
 	
-	public Long getTotal() {
-	return sqlSession.selectOne(NAMESPACE+"getTotal");
-	}
-	public List<ProductDTO> list(Pager pager) throws Exception {
-	return sqlSession.selectList(NAMESPACE+"list",pager);
-	}
 	
+	public Long getTotal() {
+		return sqlSession.selectOne(NAMESPACE + "getTotal");
+	}
+
+	public List<ProductDTO> list(Pager pager) throws Exception {
+		return sqlSession.selectList(NAMESPACE + "list", pager);
+	}
+
 	public ProductDTO detail(ProductDTO productDTO) throws Exception {
-	return sqlSession.selectOne(NAMESPACE+"detail", productDTO);
+		return sqlSession.selectOne(NAMESPACE + "detail", productDTO);
 	}
+
 	public int add(ProductDTO productDTO) throws Exception {
-	return sqlSession.insert(NAMESPACE+"add",productDTO);
+		return sqlSession.insert(NAMESPACE + "add", productDTO);
 	}
+
 	public int update(ProductDTO productDTO) throws Exception {
-	return sqlSession.update(NAMESPACE+"update",productDTO);
+		return sqlSession.update(NAMESPACE + "update", productDTO);
 	}
 }
