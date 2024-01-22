5b2af7e0201607b1618eacee71d8451ff0e983b8
dirokim <dirokim7@gmail.com>
2024-01-22 17:51:02 +0900

    ad

--- src/main/webapp/resources/js/agreement.js
+++ src/main/webapp/resources/js/agreement.js
@@ -2,6 +2,38 @@
 const all  = document.getElementById("checkAll");
 const checks = document.getElementsByClassName("checks");
 all.addEventListener("click",()=>{
-        checks.setAttribute("checked","check");
-        
+       let v = all.getAttribute("checked");
+        console.log("check:",v);
+        v= all.checked;
+        for(let check of checks){
+            check.checked = v;
+        }
+
+   
  })
+
+ for(let c of checks){
+    c.addEventListener("click",function(){
+        console.log(this.checked)
+        if(this.checked==false){
+            all.checked = false;
+        }else if(checks!=false){
+            all.checked = true;
+        }
+
+    })
+ }
+
+//  $('#checkAll').click(()=>{
+//    let v =  $("#checkAll").prop("checked");
+//     $('.checks').prop("checked",v);
+
+// })
+
+// $('.checks').click(function(){
+//     if($(this.prop('checked')==false)){
+//         $("#checkAll").prop("checked",true)
+//     }else if ($("#checkAll").prop('checked')!=false){
+//         $("#checkAll").prop("checked",false)
+//     }
+// })
