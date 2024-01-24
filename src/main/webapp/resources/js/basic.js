f382cb58fa3e04327cccb63cb51908ce348e3851
dirokim <dirokim7@gmail.com>
2024-01-24 21:05:33 +0900

    ad

--- src/main/webapp/resources/js/basic.js
+++ src/main/webapp/resources/js/basic.js
@@ -1,35 +1,42 @@
-
 let btn = document.getElementById("btn");
 let btn2 = document.getElementById("btn2");
 
-btn2.addEventListener("click",function(){
+btn2.addEventListener("click", function(){
     let t = document.getElementById("title").value;
     let c = document.getElementById("contents").value;
-    fetch("/notice/add",{
+
+    fetch("/notice/add", {
         method:"POST",
         headers:{
             "Content-type":"application/x-www-form-urlencoded"
         },
-        body:"noticeHead="+t+"&noticeContents="+c
-    }).then(response=> response.text())
-    .then(response=>console.log(response));
+        body:"boardTitle="+t+"&boardContents="+c
+    })
+    .then(response => response.text())
+    .then(response => console.log(response));
     
+    ;
+
 })
 
-btn.addEventListener("click",()=>{
-    
+
+
+btn.addEventListener("click", function(){
     console.log("Ajax 시작");
 
-    fetch("/notice/list",{
-        method:"GET",
-       
-
-    }).then(response=>{return response.text()})
+    fetch("/notice/list", {
+        method:"GET"
+    })
+    .then(response=>{return response.text()})
     .then((res)=>{
         document.getElementById("result").innerHTML=res;
-        console.log(res);
-    });
 
-    console.log("Ajax 끝");
+    })
+    //Arrow Function
+    //function(){}  -> ()=>console.log
+    
+    ;
+
+    console.log("끝");
 
 })
